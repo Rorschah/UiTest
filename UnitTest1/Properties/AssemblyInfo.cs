b27123a4be16f498e90f1b9a129acc788dafb995
David Kucera <d_kucera@seznam.cz>
2020-08-11 20:33:13 +0200

    Add All files

---
+++ UnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dbbef4ea-2d7b-457a-bd72-fc3f0fbaddbf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Open the app
+            var application = Application.Launch(@"C:\Program Files\IDEA StatiCa\StatiCa 20.0\IDEAStatiCa.exe");
+
+            // Finds the main window 
+            var window = application.GetWindow("IDEA StatiCa");
+
+            // Find the button
+            var closeButton = window.Get<Button>("PART_Close");
+
+            // Click button
+            closeButton.Click();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // Open the app
+            var application = Application.Launch(@"C:\Program Files\IDEA StatiCa\StatiCa 20.0\IDEAStatiCa.exe");
+
+            // Finds the main window 
+            var window = application.GetWindow("IDEA StatiCa");
+
+
+            //// Find the button - takto to nefunguje!!
+            //var steelBtn = window.Get<RadioButton>("STEEL");
+
+            //// Click button
+            //steelBtn.Click();
+
+            // Find the button
+            var steelButton = window.Get<RadioButton>(SearchCriteria.ByText("STEEL"));
+            steelButton.Click();
+
+            
+
+            
+
+
+
+
+        }
+
+    }
+}
